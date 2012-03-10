3b75e93ea025ac30b5fd7b5d82903ae9e69048b9
Justin Bachorik <justinbach@gmail.com>
2012-03-10 11:02:29 -0500

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Snapfood::Application.config.session_store :cookie_store, key: '_snapfood_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Snapfood::Application.config.session_store :active_record_store
