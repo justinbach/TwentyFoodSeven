e32913b4ed14b95118c18fb98cba27a52c4c3d1e
Justin Bachorik <justinbach@gmail.com>
2012-03-10 16:09:32 -0500

    generated devise views

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ TwentyFoodSeven::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'foodsnaps#index'
+  root :to => 'meals#index'
 
   # See how all your routes lay out with "rake routes"
 
