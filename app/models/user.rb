fbd1e1e7d09228cdd07038e0064ac30c2b113276
Justin Bachorik <justinbach@gmail.com>
2012-03-11 10:44:12 -0400

    converted devise views to haml

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  validates_presence_of :name
+
 end
