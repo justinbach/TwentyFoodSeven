c9cdd700d2860cc694f4f850f40c68b129cbb058
Justin Bachorik <justinbach@gmail.com>
2012-03-17 09:17:47 -0400

    photos integrated into show, edit, new, list views

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,7 +4,7 @@ attr_protected :user_id
 
 belongs_to :user
 
-validates_presence_of :name, :meal_time
+validates_presence_of :name, :meal_time, :meal_photo
 
 mount_uploader :meal_photo, MealPhotoUploader
 
