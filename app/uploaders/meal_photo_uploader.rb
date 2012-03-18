3983959a5a5d6507f7bcbf83f48683cbe7b826f1
Justin Bachorik <justinbach@gmail.com>
2012-03-18 09:18:23 -0400

    reconfigured carrierwave's storage directories in the uploader

--- app/uploaders/meal_photo_uploader.rb
+++ app/uploaders/meal_photo_uploader.rb
@@ -8,14 +8,22 @@ class MealPhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
